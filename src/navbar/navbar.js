2784db2c88f4f812759782ea1889bff219e4e5b6
hegnemurd <hegne.murd@gmail.com>
2023-09-05 21:08:10 +0100

    Creating navbar and starting to style it

--- src/navbar/navbar.js
+++ src/navbar/navbar.js
@@ -1,17 +1,59 @@
-import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import {
+  Button,
+  Container,
+  Form,
+  Nav,
+  Navbar,
+  Offcanvas,
+} from "react-bootstrap";
 
 function AppNavbar() {
   return (
-    <Navbar className="bg-body-tertiary">
-      <Container>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Nav className="me-auto">
-          <NavDropdown id="basic-nav-dropdown">
-            <NavDropdown.Item>Cart</NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-      </Container>
-    </Navbar>
+    <div>
+      {[false].map((expand) => (
+        <Navbar key={expand} expand={expand} className="bg-body-tertiary mb-3">
+          <Container fluid>
+            <Navbar.Brand href="#">Nav Offcanvas</Navbar.Brand>
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${expand}`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+              placement="end"
+              title={<i class="bi bi-cart"></i>}
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                  Offcanvas
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3">
+                  <Container id="cart">
+                    <Button id="delButton">X</Button>
+                    <img alt="clothing-item" />
+                    <h3>Clothing Item Description</h3>
+                    <p>size | colour</p>
+                    <p>quantity</p>
+                    <p>cost $</p>
+                    <p>- +</p>
+                  </Container>
+                  <Button variant="outline-primary">Checkout</Button>
+                </Nav>
+                <Form className="d-flex">
+                  <Form.Control
+                    type="Search"
+                    placeholder="Search"
+                    className="me-2"
+                    aria-label="Search"
+                  />
+                  <Button variant="outline-success">Search</Button>
+                </Form>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+      ))}
+    </div>
   );
 }
 
