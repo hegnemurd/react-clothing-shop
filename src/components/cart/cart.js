4d2a694592c535d2740828a33539a6ddb936b71a
hegnemurd <hegne.murd@gmail.com>
2023-09-16 13:43:50 +0100

    Adding more styling and starting to work on the cart

--- src/components/cart/cart.js
+++ src/components/cart/cart.js
@@ -4,14 +4,24 @@ function Cart() {
   return (
     <div>
       <Container id="cart">
-        <CloseButton className="delButton" />
-        {/* <Button className="delButton">X</Button> */}
-        <img alt="clothing-item" />
-        <h3>Clothing Item Description</h3>
-        <p>size | colour</p>
-        <p>quantity</p>
-        <p>cost $</p>
-        <p>- +</p>
+        <div>
+          <CloseButton className="delButton" />
+          Cart icon - Cart
+        </div>
+
+        <div>
+          <img alt="clothing-item" />
+          <h3>Clothing Item Description</h3>
+          <p>size | colour</p>
+          <p>quantity</p>
+          <p>cost $</p>
+          <p>- +</p>
+        </div>
+
+        <div>
+          <div>Subtotal cost discount?</div>
+          <div>checkout</div>
+        </div>
       </Container>
       <Button variant="outline-primary">Checkout</Button>
     </div>
